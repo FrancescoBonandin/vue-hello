5d2677805f09e934438d259cb148b0368843bb20
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-30 15:55:10 +0200

    EDIT completato

---
+++ js/my-script.js
@@ -0,0 +1,13 @@
+
+const{createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            message:"Hey,Evan You, I love Vue",
+            lOL:"LOL",
+            evanYou:"img/Evan.jpg",
+            bsTextCenter:"text-center"
+        }
+    }
+}).mount("#app")
